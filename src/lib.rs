80d98fd7dbf0833879826175f9abaf15e6d0bad0
Tarik Eshaq <teshaq@mozilla.com>
2020-05-15 11:01:03 -0700

    adds supporting intervals
    - Uses configstore to store latest checked timestamp
    - Adds tests
    
    uses directories to support windows
    
    uses platform-dirs
    
    use = in the edge case that duration is super small (unlikely)
    
    Uses serialize and deserialize directly on DateTime to avoid any issues with to_string
    
    compare DateTime directly and remove .clone()
    
    Fix making unique file names for crates (Facepalm)
    
    add 'the'
    
    Compare timestamps in test, use chrono DateTime for comparison and read json directly from reader
    
    Adds proper setup for tests
    
    remove file creation in test
    
    Uses trucate function on OpenOptions
    
    Remove race conditions between tests
    
    consume new configstore instead of platforms-rs
    
    modify name of app to be -update-notifier

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
 use ansi_term::Color::{Blue, Green, Red, Yellow};
+use chrono::{DateTime, Utc};
+use configstore::{AppUI, Configstore};
 use reqwest::blocking::Client;
 use reqwest::header;
-use serde_derive::Deserialize;
+use serde_derive::{Deserialize, Serialize};
+use std::time::Duration;
 
 const REGISTRY_URL: &str = "https://crates.io";
 
@@ -26,6 +29,8 @@ pub enum ErrorKind {
     UnableToParseJson(String),
     #[error("Error received from registry: {0}")]
     RegistryError(String),
+    #[error("Unable to find config")]
+    UnableToFindConfig,
 }
 
 #[derive(Deserialize, Debug, Clone)]
@@ -115,15 +120,64 @@ fn print_notice(name: &str, current_version: &str, latest_version: &str) {
     print!("{}", generate_notice(name, current_version, latest_version));
 }
 
-/// Validates current version of crate
-/// Takes the current name and version
-/// Prints directly to stdout (Will probably change to be more asynchrounos)
-pub fn check_version(name: &str, current_version: &str) -> Result<(), Box<dyn std::error::Error>> {
+#[derive(Deserialize, Serialize)]
+struct Config {
+    last_checked: DateTime<Utc>,
+}
+
+fn get_app_name(name: &str) -> String {
+    let mut app_name: String = String::from(name);
+    app_name.push_str("-update-notifier");
+    #[cfg(test)]
+    app_name.push_str("-test");
+    app_name
+}
+
+fn update_time(date_time: DateTime<Utc>, name: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let config = Config {
+        last_checked: date_time,
+    };
+    let config_store = Configstore::new(&get_app_name(name), AppUI::CommandLine)?;
+    config_store.set("config", config)?;
+    Ok(())
+}
+
+fn compare_with_latest(
+    name: &str,
+    current_version: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
     let latest_version = get_latest_version(name)?;
     if latest_version != current_version {
         print_notice(name, current_version, &latest_version);
     }
-    Ok(())
+    let date_time = Utc::now();
+    update_time(date_time, name)
+}
+
+/// Validates current version of the crate
+/// Takes the current name and version
+/// Takes a std::time::Duration to determine the interval
+///         So that it does not notify the user every run
+/// Prints directly to stdout (Will probably change to be more asynchrounos)
+pub fn check_version(
+    name: &str,
+    current_version: &str,
+    interval: Duration,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let date_time_now = Utc::now();
+    let config_store = Configstore::new(&get_app_name(name), AppUI::CommandLine)?;
+    match config_store.get::<Config>("config") {
+        Ok(config) => {
+            let prev_time = config.last_checked;
+            let duration_interval = chrono::Duration::from_std(interval)?;
+            if date_time_now.signed_duration_since(prev_time) >= duration_interval {
+                compare_with_latest(name, current_version)
+            } else {
+                Ok(())
+            }
+        }
+        Err(_) => compare_with_latest(name, current_version),
+    }
 }
 
 #[cfg(test)]
